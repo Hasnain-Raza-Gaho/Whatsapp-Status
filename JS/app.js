e39373d302a5fe11106c55b9af7bf3e85eec5944
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2021-10-07 07:23:44 +0500

    2 buttons added

--- JS/app.js
+++ JS/app.js
@@ -2,14 +2,46 @@ document.getElementById('textField').addEventListener('input',textWrite);
 
 
 function textWrite(){
-    document.getElementById('post').textContent = document.getElementById('textField').value;
+    document.getElementById('post').innerText = document.getElementById('textField').value;
 }
 
 
-function uppercase(){
-document.getElementById('post').style.textTransform = 'uppercase'
+function textTransform() {
+var select = document.getElementById('textTransform');
+var option = select.options[select.selectedIndex]
+// console.log(option.value)
+
+if (option.value == 'uppercase') {
+    document.getElementById('post').style.textTransform = 'uppercase';
+
+} else if(option.value == 'capitalize') {
+    document.getElementById('post').style.textTransform = 'capitalize'
+    
+}
+ else if(option.value == 'empty') {
+    document.getElementById('post').style.textTransform = ''
+    
+}
+    
 }
 
-function capitalize(){
-document.getElementById('post').style.textTransform = 'Capitalize'
+
+
+function textAlign() {
+var select = document.getElementById('textAlign');
+var option = select.options[select.selectedIndex]
+// console.log(option.value)
+
+if (option.value == 'left') {
+    document.getElementById('post').style.textAlign = 'left'
+
+} else if(option.value == 'right') {
+    document.getElementById('post').style.textAlign = 'right'
+    
+}
+ else if(option.value == 'center') {
+    document.getElementById('post').style.textAlign = 'center'
+    
+}
+    
 }
